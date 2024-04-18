ac5cd38d6858f53e0de577f17be2ad20a479b71b
Maksym Martynenko <maxerdy@gmail.com>
2024-04-18 07:06:03 +0200

    Split the GUI and GUI functions into two separate classes in preparation for more GUI changes.

---
+++ mainWindow.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 06:24:31 2024
+
+@author: maxer
+"""
+
+from PyQt6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QDialog, QGridLayout
+from PyQt6.QtGui import QIcon
+from windowHelper import windowHelper
+
+class mainWindow:
+    
+    def __init__(self):
+        self.helper = windowHelper(self)
+        self.initWindow()
+        self.initMapSelector()
+        self.initPackSelector()
+        self.initResMsg()
+        self.initExecuteButton()
+        self.initExportButton()
+        self.initExportSettings()
+        
+    def run(self):
+        self.window.show()
+        
+    def initWindow(self):
+        self.window = QWidget()
+        self.window.setWindowTitle("WCIII Content Manager")
+        self.icon = QIcon("w3cm.ico")
+        self.window.setWindowIcon(self.icon)
+        self.window.setGeometry(100, 100, 900, 800)
+        self.header = QLabel("<h1>Warcraft III Content Manager</h1>", parent=self.window)
+        self.header.move(60, 15)
+        
+    def initMapSelector(self):
+        
+        self.mapsPathInput = QLineEdit(parent=self.window)
+        self.mapsPathInput.setGeometry(220, 100, 600, 30)
+        self.selectMapsButton = QPushButton(text='Select Map Files', parent=self.window)
+        self.selectMapsButton.setGeometry(30, 100, 180, 30)
+        self.selectMapsButton.clicked.connect(self.helper.selectMapsFunction)
+        
+    def initPackSelector(self):
+        
+        self.packSelectorSpace = QWidget(parent=self.window)
+        geometry, layout = self.helper.importContentPacks()
+
+        self.packSelectorSpace.setGeometry(*geometry)
+        self.packSelectorSpace.setLayout(layout)
+        
+    def initResMsg(self):
+        
+        self.resultMsg = QLabel("", parent=self.window)
+        self.resultMsg.setGeometry(450-110, 660, 220, 40)
+        
+    def initExecuteButton(self):
+        
+        self.executeButton = QPushButton(text='Apply Content Packs', parent=self.window)
+        self.executeButton.setGeometry(340-220, 720, 220, 40)
+        self.executeButton.clicked.connect(self.helper.executeFunction)
+        
+    def initExportButton(self):
+        
+        self.exportButton = QPushButton(text='Export Content', parent=self.window)
+        self.exportButton.setGeometry(340+220, 720, 220, 40)
+        self.exportButton.clicked.connect(self.helper.selectMapFunction)
+        
+    def initExportSettings(self):
+        
+        self.settingsDialog = QDialog()
+        self.settingsDialog.setGeometry(100, 100, 700, 880)
+        self.settingsLayout = QGridLayout()
+        self.settingsListSpace = QWidget(parent=self.settingsDialog)
+        self.settingsListSpace.setLayout(self.settingsLayout)
+        self.settingsDialogOk = QPushButton(text = 'Confirm', parent=self.settingsDialog)
+        self.settingsDialogOk.setGeometry(350-40, 820, 80, 40)
+        self.settingsDialogOk.clicked.connect(self.settingsDialog.accept)
+        self.settingsDialog.accepted.connect(self.helper.finalizeContentPack)
